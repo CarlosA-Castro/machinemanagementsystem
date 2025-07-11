d98cd805bfcce1a197ad6ef045314045769afc12
unknown <carlosacs2004@gmail.com>
2025-07-10 19:24:59 -0500

    Primer commit del sistema de gestión de máquinas

---
+++ maquinas-medellin-frontend/public/particles-config.js
@@ -0,0 +1,41 @@
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: "#ffffff"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.5,
+      random: false
+    },
+    size: {
+      value: 3,
+      random: true
+    },
+    move: {
+      enable: true,
+      speed: 3,
+      direction: "none",
+      out_mode: "out"
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      }
+    }
+  },
+  retina_detect: true
+});
+
